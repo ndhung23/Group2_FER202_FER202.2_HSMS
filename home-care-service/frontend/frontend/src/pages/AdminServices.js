eb560cdd18c26ef42a1a379a3c231accf14cdc12
Zoxy <139949025+ndhung23@users.noreply.github.com>
2026-03-04 03:07:49 +0700

    Revert "Nguyễn duy hưng   he190591"

--- home-care-service/frontend/frontend/src/pages/AdminServices.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AdminServices() {
-  return (
-    <div>AdminServices</div>
-  )
-}
-
-export default AdminServices
--- home-care-service/frontend/frontend/src/pages/HelperDashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HelperDashboard() {
-  return (
-    <div>HelperDashboard</div>
-  )
-}
-
-export default HelperDashboard
